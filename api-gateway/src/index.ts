8b99f282ce33402f937e67c564ab2dfcd12c1582
manik <maniksarker652@gmail.com>
2026-01-17 12:20:40 +0600

    set config.json for api gateway

---
+++ api-gateway/src/index.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+// security middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 mins
+  max: 100,
+  handler: (req, res) => {
+    res.status(429).json({ message: "Too many requests, please try again letter" });
+  },
+});
+
+app.use("/api", limiter);
+
+// request handler
+app.use(morgan("dev"));
+app.use(express.json());
+
+//TODO: auth middleware
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`API gateway is running on port ${PORT}`);
+});
+
+// 404 handler
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+// Error handler
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal service error" });
+});
